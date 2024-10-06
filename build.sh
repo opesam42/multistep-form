10bd99d383d212498f9c76046f25f7d033c8545e
Gbenga Opeyemi <opesam42@gmail.com>
2024-10-06 23:35:18 +0100

    Update vercel.json and build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+# build.sh
+echo "Starting build script"
+
+pip install -r requirements.txt
+
+# make migrations
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# collectstatic
+python3 manage.py collectstatic
+
+echo "Build script completed"
