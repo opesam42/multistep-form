50b219a42b90521c9079682bae3e571ff16e1d75
Gbenga Opeyemi <opesam42@gmail.com>
2024-10-06 23:09:48 +0100

    changes to start.sh2

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #create a virtual env
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 #installing pip
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # Run migrations
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Start the Django application
 # gunicorn gamingform.wsgi:application --bind 0.0.0.0:$PORT --timeout 120
